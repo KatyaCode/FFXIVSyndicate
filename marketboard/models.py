1360fb394ce75aa17055b974ff27847024834b59
Katya <katyacode@gmail.com>
2018-08-16 00:32:39 -0700

    Add model_utils, switch to TimeStampedModel with choicefield for server

--- marketboard/models.py
+++ marketboard/models.py
@@ -1,9 +1,87 @@
-from django.db import models
+from enum import Enum
 
-class Transaction(models.Model):
+from django.db import models
+from model_utils.models import TimeStampedModel
+from model_utils import Choices
+
+SERVER_LIST = Choices(
+    'Aegis',
+    'Atomos',
+    'Carbuncle',
+    'Garuda',
+    'Gungnir',
+    'Kujata',
+    'Ramuh',
+    'Tonberry',
+    'Typhon',
+    'Unicorn',
+    'Alexander',
+    'Bahamut',
+    'Durandal',
+    'Fenrir',
+    'Ifrit',
+    'Ridill',
+    'Tiamat',
+    'Ultima',
+    'Valefor',
+    'Yojimbo',
+    'Zeromus',
+    'Anima',
+    'Asura',
+    'Belias',
+    'Chocobo',
+    'Hades',
+    'Ixion',
+    'Mandragora',
+    'Masamune',
+    'Pandemonium',
+    'Shinryu',
+    'Titan',
+    'Adamantoise',
+    'Balmung',
+    'Cactuar',
+    'Coeurl',
+    'Fairy',
+    'Gilgamesh',
+    'Goblin',
+    'Jenova',
+    'Mateus',
+    'Midgardsormr',
+    'Sargatanas',
+    'Siren',
+    'Zalera',
+    'Behemoth',
+    'Brynhildr',
+    'Diabolos',
+    'Excalibur',
+    'Exodus',
+    'Famfrit',
+    'Hyperion',
+    'Lamia',
+    'Leviathan',
+    'Malboro',
+    'Ultros',
+    'Cerberus',
+    'Lich',
+    'Louisoix',
+    'Moogle',
+    'Odin',
+    'Omega',
+    'Phoenix',
+    'Ragnarok',
+    'Shiva',
+    'Zodiark',
+)
+
+
+class Transaction(TimeStampedModel):
     item_id = models.IntegerField()
     quantity = models.IntegerField()
     quality = models.BooleanField()
     price = models.IntegerField()
     transaction_time = models.DateTimeField()
     buyer = models.CharField(max_length=21)
+    server = models.CharField(
+        max_length=12,
+        choices=SERVER_LIST
+    )
--- marketboard/tests/test_models.py
+++ marketboard/tests/test_models.py
@@ -1,19 +1,41 @@
+from contextlib import contextmanager
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-from django.db import IntegrityError, DataError
 from django.utils import timezone
 
-from marketboard.models import Transaction
+from marketboard.models import Transaction, SERVER_LIST
 
 
 class TransactionModelTest(TestCase):
 
+    @contextmanager
+    def assertValidationErrors(self, fields):
+        """
+        Assert that a validation error is raised, containing all the specified fields,
+        and only the specified fields.
+        """
+        try:
+            yield
+            raise AssertionError('ValidationError not raised')
+        except ValidationError as e:
+            self.assertEqual(set(fields), set(e.message_dict.keys()))
+
     def test_cannot_create_empty_transaction(self):
         transaction = Transaction()
-        with self.assertRaises(IntegrityError):
-            transaction.save()
+        with self.assertValidationErrors(['item_id', 'quantity', 'quality', 'price',
+                                          'transaction_time', 'buyer', 'server']):
+            transaction.full_clean()
 
     def test_cannot_create_transaction_with_invalid_buyer_name(self):
-        transaction = Transaction(item_id=1, quantity=1, quality=False, price=1,
+        transaction = Transaction(item_id=1, quantity=1, quality=False, price=1, server='Ultros',
                                   transaction_time=timezone.now(), buyer='A name that is very much too long')
-        with self.assertRaises(DataError):
-            transaction.save()
+        with self.assertValidationErrors(['buyer']):
+            transaction.full_clean()
+
+    def test_cannot_save_transaction_with_invalid_server_choice(self):
+        transaction = Transaction(item_id=1, quantity=1, quality=False, price=1,
+                                  transaction_time=timezone.now(), buyer='Shovel Akaza',
+                                  server='Cloud')
+        with self.assertValidationErrors(['server']):
+            transaction.full_clean()
