4ea6915ed9eb6448c02a6902a9f876c3bb9863fa
Katya <katyacode@gmail.com>
2018-08-23 12:58:48 -0700

    Extended Functional site navigation test to include server home page

--- functional_tests/test_site_navigation.py
+++ functional_tests/test_site_navigation.py
@@ -1,15 +1,50 @@
+import time
+
+from django.utils import timezone
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+
+from marketboard.tests.factories import TransactionFactory
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        TransactionFactory(
+            item_id='111',
+            transaction_time=timezone.now(),
+            server='Ultros'
+        )
+        TransactionFactory(
+            item_id='444',
+            transaction_time=timezone.now(),
+            server='Hyperion'
+        )
 
     def tearDown(self):
         self.browser.refresh()
         self.browser.quit()
+
+    def wait(fn):
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+
+        return modified_fn
+
+    @wait
+    def wait_for(self, fn):
+        return fn()
 
     def test_can_visit_and_navigate_website(self):
         # Shovel has heard about a new WebApp for making tons of gil and visits the url
@@ -24,4 +59,16 @@ class NewVisitorTest(StaticLiveServerTestCase):
         ultros_link = self.browser.find_element_by_link_text('Ultros')
         ultros_link.click()
 
-        self.fail('Finish Writing')
+        # He finds himself on a page that looks specific to the Ultros server's market activity
+        self.wait_for(lambda: self.assertIn('Ultros',
+                                            self.browser.find_element_by_tag_name('h1').text))
+
+        # He sees a list of recent item transactions specific to the Ultros server
+        recent_transactions = self.browser.find_element_by_id('recentTransactions')
+        self.assertIn('111', recent_transactions)
+        self.assertNotIn('404', recent_transactions)
+
+        # Interested in what else he can learn, he clicks on the link to the
+        # item details within the recent transactions table
+        details_link = self.browser.find_element_by_link_text('111')
+        details_link.click()
