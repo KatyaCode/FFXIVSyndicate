180af700510822b145733f49cf5bcb03f21e2f1e
Katya <katyacode@gmail.com>
2018-08-23 17:54:33 -0700

    Complete basic site navigation FT

--- functional_tests/test_site_navigation.py
+++ functional_tests/test_site_navigation.py
@@ -17,12 +17,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
         TransactionFactory(
             item_id='111',
             transaction_time=timezone.now(),
-            server='Ultros'
+            server='Ultros',
+            buyer='Katya Dankri'
         )
         TransactionFactory(
             item_id='444',
             transaction_time=timezone.now(),
-            server='Hyperion'
+            server='Hyperion',
         )
 
     def tearDown(self):
@@ -72,3 +73,28 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # item details within the recent transactions table
         details_link = self.browser.find_element_by_link_text('111')
         details_link.click()
+
+        # He's presented with a page showing the details of the item on Ultros,
+        # listing recent transactions on that server involving that item, including the buyer.
+        self.wait_for(lambda: self.assertIn('111',
+                                            self.browser.find_element_by_tag_name('h1').text))
+        recent_item_transactions = self.browser.find_element_by_id('recentTransactions')
+        self.assertNotIn('444', recent_item_transactions)
+        self.assertIn('111', recent_item_transactions)
+        self.assertIn('Katya Dankri', recent_item_transactions)
+
+        # Recognizing a familiar name, he clicks on the buyer link to see what his
+        # friend has been purchasing
+
+        buyer_link = self.browser.find_element_by_link_text('Katya Dankri')
+        buyer_link.click()
+
+        # He gets taken to another page showing a list of all transactions performed by
+        # that buyer, including the item that he just navigated to that page from
+        self.wait_for(lambda: self.assertIn('Katya Dankri',
+                                            self.browser.find_element_by_tag_name('h1').text))
+        recent_buyer_transactions = self.browser.find_element_by_id('recentTransactions')
+        self.assertIn('111', recent_buyer_transactions)
+
+        # Noting how useful the sight might be for him, Shovel closes his browser
+        # and goes back to watching anime.
